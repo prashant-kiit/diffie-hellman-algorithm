1b607fc2ba19c43e78c6b2b803d6e9dea823ecaf
prashant-m4 <prashantsingh090798@gmail.com>
2025-08-10 01:02:36 +0530

    feat: added login form

---
+++ frontend/src/App.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+function App() {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Form submitted:", formData);
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} >
+        <h5>LOGIN</h5>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
+          required
+          autoComplete="username"
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="text"
+          id="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+          autoComplete="current-password"
+        />
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
+}
+
+export default App;
