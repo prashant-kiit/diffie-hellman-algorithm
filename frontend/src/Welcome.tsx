bb48998d2c79992359cdd646c519e99ede5e6ecb
prashant-m4 <prashantsingh090798@gmail.com>
2025-08-10 17:41:58 +0530

    feat: added content table

--- frontend/src/Welcome.tsx
+++ frontend/src/Welcome.tsx
@@ -1,3 +1,7 @@
+import { useEffect, useState } from "react";
+
+type Users = { username: string; password: string }[] | [];
+
 function Welcome({
   isLoggedIn,
   setIsLoggedIn,
@@ -5,33 +9,59 @@ function Welcome({
   isLoggedIn: boolean;
   setIsLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
 }) {
-  async function handleLogout(
-    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ): Promise<void> {
-    e.preventDefault();
+  const [users, setUsers] = useState<Users>([]);
+
+  useEffect(() => {
+    (async () => {
+      await handleGetUsers();
+    })();
+  }, []);
+
+  async function handleLogout(): Promise<void> {
     const response = await fetch("http://localhost:8000/logout", {
       method: "GET",
       credentials: "include",
     });
 
-    if(response.ok) {
+    if (response.ok) {
       const result = await response.json();
       alert(result.message);
       if (isLoggedIn) setIsLoggedIn(false);
     }
   }
 
-  function handleGetUsers(
-    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ): void {
-    e.preventDefault();
-    console.log(document.cookie);
+  async function handleGetUsers(): Promise<void> {
+    const response = await fetch("http://localhost:8000/users", {
+      method: "GET",
+      credentials: "include",
+    });
+
+    if (response.ok) {
+      const result = await response.json();
+      console.log(result?.message);
+      setUsers(result?.message);
+    }
   }
 
   return (
     <div className="welcome">
       <h3>Welcome</h3>
-      <p>Content</p>
+      <table border={1}>
+        <thead>
+          <tr>
+            <th>Username</th>
+            <th>Password</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users?.map((user, index) => (
+            <tr key={index}>
+              <td>{user?.username}</td>
+              <td>{user?.password}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
       <div className="button-box">
         <button onClick={handleLogout}>Logout</button>
         <button onClick={handleGetUsers}>Users</button>
